e0546327a564c2a0181b9a3a2228a35a349f0cdf
mohamedwally2035 <mohamedwally87@gmail.com>
2024-03-13 18:38:34 -0700

    a

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end= ")
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{:c}".format(i), end="")
