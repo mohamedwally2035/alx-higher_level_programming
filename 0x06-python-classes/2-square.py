eb68ba812d55b1c1cddbd2b998e90dcba29e3313
mohamedwally2035 <mohamedwally87@gmail.com>
2024-09-15 12:35:37 -0700

    a

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,10 @@
 
 
 class Square:
-    """ Square class"""
+    """ Square class """
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
